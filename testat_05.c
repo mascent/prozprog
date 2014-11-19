ab1e1aa71f7eb1458113a29f32fc888baf2f17c8
mascent <vincent@innovandalism.eu>
2014-11-19 08:42:16 +0100

    Finished
    usage:
     - select field by colum-row
     - select action witch s (select) or f (plant flag/remove flag)
     - e.g:
      - a0s
      - b7f
      - c5s
    Winning if all mines are shown with a flag and no other flag exists

--- testat_05.c
+++ testat_05.c
@@ -1,6 +1,6 @@
 /*Originial provied by mascent
 
-textbased implimentation of minesweeper not finished yet
+textbased implimentation of minesweeper
 */
 
 #include <stdio.h>
@@ -8,20 +8,39 @@ textbased implimentation of minesweeper not finished yet
 #include <stdlib.h>
 #include <math.h>
 
-#define fields 9
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KMAG "\x1B[35m"
+#define KCYN "\x1B[36m"
+#define KWHT "\x1B[37m"
 
-int field_value[fields][fields];
+#define fields_x 9
+#define fields_y 9
 
-int intial_field (void){
 
-	for (int i = 0; i < fields; ++i)
+char zuordnung[16] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'};
+int field_value[fields_y][fields_x];
+int sieg = 0;
+int lose_win = 0;
+int flag = 0;
+int bomb_n;
+
+
+int initial_field(void){
+	//printf("Debugger: wurde aufgerufen!\n");
+	for (int i = 0; i < fields_y; ++i)
 	{
-		for (int j = 0; j < fields; ++j)
+		for (int j = 0; j < fields_x; ++j)
 		{
 			field_value[i][j] = -11;
+			//printf("%d\t%d\t%d\n", i, j, field_value[i][j]);
 		}
 	}
 
+	return 0;
 }
 
 int generate_mines(int n){
@@ -31,8 +50,8 @@ int generate_mines(int n){
 
 	do
 	{
-		x = (rand() % fields);
-		y = (rand() % fields);
+		x = (rand() % fields_x);
+		y = (rand() % fields_y);
 
 		if (field_value[y][x] != -10)
 		{
@@ -40,11 +59,11 @@ int generate_mines(int n){
 			//printf("True(%d): %d\t%d\n", i, y, x);
 			for (int k = y-1; k <= y+1; ++k)
 			{
-				if (k > -1 && k < fields)
+				if (k > -1 && k < fields_y)
 				{
 					for (int l = x - 1; l <= x+1; ++l)
 					{
-						if (l > -1 && l < fields && field_value[k][l] != -10)
+						if (l > -1 && l < fields_x && field_value[k][l] != -10)
 						{
 							if (field_value[k][l] > -10){
 								field_value[k][l] -= 1;
@@ -60,27 +79,36 @@ int generate_mines(int n){
 		} else {
 			//printf("False(%d): %d\t%d\n", i, y, x);
 		}
-	} while (i < fields);
+	} while (i < n);
 	return 0;
 }
 
 int print_field(void){
 	printf("\n");
-	for (int i = 0; i < fields; ++i)
+	for (int i = -1; i < fields_y; ++i)
 	{
-		for (int j = 0; j < fields; ++j)
+		for (int j = -1; j < fields_x; ++j)
 		{
-			if (field_value[i][j] < 0){
-				printf("[]\t");
-			}
-			else if (field_value[i][j] == 10){
-				printf("[X]\t");
-			} else if (field_value[i][j] == 11)
-			{
-				printf("[.]\t");
-			} else {
-				printf("[%d]\t", field_value[i][j]);
-			}
+				if (i < 0 && j > -1) {
+					printf(KRED "%d\t" KNRM, j);				//Ausgabe der Achsenbeschriftung
+				} else if (j < 0 ) {
+					printf(KRED "%c\t" KNRM, zuordnung[i]);		//Ausgabe der Achsenbeschriftung
+				}
+				else if (field_value[i][j] < 0 && field_value[i][j] > -12){
+					printf("[]\t");
+				}
+				else if (field_value[i][j] == 10){
+					printf(KMAG "X\t" KNRM);
+				}else if (field_value[i][j] == 11)
+				{
+					printf(KBLU ".\t" KNRM);
+				}
+				else if (field_value[i][j] < -11) {
+					printf("[F]\t");
+				}
+				else {
+					printf("%d\t", field_value[i][j]);
+				}
 		}
 		printf("\n");
 	}
@@ -89,18 +117,21 @@ int print_field(void){
 int open_field(int x, int y){
 	int help;
 	//printf("%d\t%d\t%d\n", x, y, field_value[y][x]);
-	if (field_value[y][x] < 0) { 
+	if (field_value[y][x] < 0 && field_value[y][x] > -12) { 
 		help = field_value[y][x]; 
 		field_value[y][x] = -help;
-		printf("%d\t%d\t%d\n", x, y, field_value[y][x]);
+		//printf("%d\t%d\t%d\n", x, y, field_value[y][x]);
 		print_field();
+		if (field_value[y][x] == 10){
+			lose_win = 2;
+		}
 		if (field_value[y][x] == 11)
 		{
 			for (int k = y-1; k < y+2; ++k)
 				{ 
 					for (int l = x - 1; l < x+2; ++l)
 						{
-							if (l > -1 && l < fields && k > -1 && k < fields)
+							if (l > -1 && l < fields_x && k > -1 && k < fields_y)
 							{
 								open_field(l ,k);
 							}
@@ -109,47 +140,106 @@ int open_field(int x, int y){
 			}
 		}
 	}
+	if (field_value[y][x] < -11)
+	{
+		//print_field();
+		printf("Entferne zunächst die Flagge um dieses Feld aufzudecken!\n");
+	}
 	return 0;
 }
 
+int set_flag (int x, int y){
+	int help;
+	if (field_value[y][x] < 0 && field_value[y][x] > -12)
+	{
+		help = field_value[y][x];
+		field_value[y][x] = help - 12;
+		flag ++;
+		if (field_value[y][x] == -22){
+			sieg ++;
+		}
+	} else if (field_value[y][x] < -11)
+	{
+		help = field_value[y][x];
+		field_value[y][x] = help + 12;
+		flag --;
+		if (field_value[y][x] == -10)
+		{
+			sieg  --;
+		}
+	} else 
+	{
+		printf("Du kannst hier keine Flagge setzen\n");
+	}
+	print_field();
+}
 
-int main(void){
-	int n = 8, mem_y, mem_x;
+int game_loop(void){
+	int mem_y, mem_x;
 	char first,second,third,fourth;
-	char zuordnung[16] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'};
 	char input[5];
 	char quit[5]; quit[0] = 'q';
 
-
-	intial_field();
-	generate_mines(n);
-	print_field();
-	//open_field(5, 5);
-
 	while (input[0] != quit[0])
 	{
-		printf(">");
-		scanf("%5s", &input[0]);
-		first = input[0]; second = input[1]; third = input[2]; fourth = input[3];
-		for (int i = 0; i < fields; ++i)
+		if (sieg == bomb_n && flag == sieg){
+			lose_win = 1;
+		}
+		if (lose_win == 0) 
 		{
-			if (first == zuordnung[i])
+			printf(">");
+			scanf("%5s", &input[0]);
+			first = input[0]; second = input[1]; third = input[2]; fourth = input[3];
+			for (int i = 0; i < fields_y; ++i)
 			{
-				mem_y = i;
+				if (first == zuordnung[i])
+				{
+					mem_y = i;
+				}
 			}
+
+			mem_x = second - '0';
+			/*if (mem_y == 4 && mem_x == 2)
+			{
+				lose_win = 1;
+			} else */
+
+			if (mem_x > -1 && mem_x < fields_x){
+				if (third == 's'){
+					open_field(mem_x,mem_y);
+				} else if (third == 'f')
+				{
+					set_flag(mem_x, mem_y);
+				}
+				//printf("%d\t%d\n", mem_y, mem_x);
+			} else {
+				printf("Die Eingabe war fehlerhaft!\n");
+			}
+
+			//printf("%c %c %c %c\n", first, second, third, fourth);
+			//printf("%s\n", input);
+		} else if (lose_win == 1)
+		{
+			printf("Du hast gewonnen!\nGood job!\n");
+			input[0] = 'q';
+		} else {
+			printf("Du hast leider verloren\n");
+			input[0] = 'q';
 		}
-
-		mem_x = second - '0';
-
-		if (mem_x > -1){
-			open_field(mem_x,mem_y);
-			printf("%d\t%d\n", mem_y, mem_x);
-		}
-
-		//printf("%c %c %c %c\n", first, second, third, fourth);
-		//printf("%s\n", input);
 	}
 
+	return 0;
+}
+
+int main(void){	
+	bomb_n = 8;//Anzahl der Bomben
+
+	initial_field();
+	generate_mines(bomb_n);
+	print_field();
+	game_loop();
+
+	//open_field(5, 5); // vorläufiger dummy test
 
 	return 0;
 }
